21cedee416ece8afda43d44d4f080f55bc9d543e
albertoncp <alberto.decela@bootcamp-upgrade.com>
2022-11-12 12:24:21 +0100

    Backend con nuestra base de datos

---
+++ src/api/ropa/ropa.model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const ropaSchema = new mongoose.Schema(
+  {
+    tipo: { type: String, trim: true, required: true },
+    color: { type: String, trim: true, required: true },
+    precio: { type: Number, trim: true, required: true },
+    imagen: { type: String, trim: true,  },
+    talla: { type: String, trim: true, required: true }
+  },
+  { timestamps: true, collection: "ropas" }
+);
+
+const Ropa = mongoose.model("ropas", ropaSchema);
+module.exports = Ropa;
